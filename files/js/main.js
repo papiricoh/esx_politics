3f8183620c2dd340c62ffd449191bd1a9e95d1e0
papiricoh <slendeman0200@gmail.com>
2022-11-08 10:14:57 +0100

    Added Vue-charts.js and charts.js

--- files/js/main.js
+++ files/js/main.js
@@ -14,6 +14,15 @@
         {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:20},
         {name: "Rodrigo", lastname:"De Borbon", party:"Oviedin", voted: false, num_votes:20}
       ],
+      v_candidates: {
+        results: [
+          {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:50},
+          {name: "Adolf", lastname:"Jk", party:"Berlin", voted: false, num_votes:420},
+          {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:20},
+          {name: "Rodrigo", lastname:"De Borbon", party:"Oviedin", voted: false, num_votes:20}
+      
+        ]
+      },
       voted: false
     }
   }
