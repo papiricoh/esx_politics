7804115d0692314bc1744fc597439ae49c2e85d9
papiricoh <slendeman0200@gmail.com>
2022-11-08 03:15:15 +0100

    added more rows to candidates list

--- files/js/main.js
+++ files/js/main.js
@@ -12,7 +12,8 @@ createApp({
         {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:0},
         {name: "Adolf", lastname:"Jk", party:"Berlin", voted: false, num_votes:0},
         {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:0}
-      ]
+      ],
+      voted: false
     }
   }
 }).mount('#app')
