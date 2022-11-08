2dcd0aeadcfc61140cdb00a5b1daca87e8c3515e
papiricoh <slendeman0200@gmail.com>
2022-11-08 09:26:05 +0100

    Added testing num_votes

--- files/js/main.js
+++ files/js/main.js
@@ -9,9 +9,9 @@ createApp({
       page: "Home",
       election: false,
       candidates: [
-        {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:0},
-        {name: "Adolf", lastname:"Jk", party:"Berlin", voted: false, num_votes:0},
-        {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:0}
+        {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:50},
+        {name: "Adolf", lastname:"Jk", party:"Berlin", voted: false, num_votes:420},
+        {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:20}
       ],
       voted: false
     }
