d0344885a6b44968a5bb68750dc5c5511b997fb2
papiricoh <slendeman0200@gmail.com>
2022-11-08 13:38:04 +0100

    Simplified js

--- files/js/main.js
+++ files/js/main.js
@@ -8,12 +8,6 @@
       count: 0,
       page: "Home",
       election: false,
-      candidates: [
-        {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:50},
-        {name: "Adolf", lastname:"Jk", party:"Berlin", voted: false, num_votes:420},
-        {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:20},
-        {name: "Rodrigo", lastname:"De Borbon", party:"Oviedin", voted: false, num_votes:20}
-      ],
       v_candidates: {
         results: [
           {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:50},
