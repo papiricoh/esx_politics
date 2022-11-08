e15debac3054c97e7cfaba4603fa895151f825a0
papiricoh <slendeman0200@gmail.com>
2022-11-08 09:42:32 +0100

    Added another row

--- files/js/main.js
+++ files/js/main.js
@@ -11,7 +11,8 @@
       candidates: [
         {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:50},
         {name: "Adolf", lastname:"Jk", party:"Berlin", voted: false, num_votes:420},
-        {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:20}
+        {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:20},
+        {name: "Rodrigo", lastname:"De Borbon", party:"Oviedin", voted: false, num_votes:20}
       ],
       voted: false
     }
