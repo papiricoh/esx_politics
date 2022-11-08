f31012fb4d3d5360932c02f0ea8df4cfe230c93e
papiricoh <slendeman0200@gmail.com>
2022-11-08 03:02:59 +0100

    Added a dynamic table

--- files/js/main.js
+++ files/js/main.js
@@ -1,11 +1,18 @@
 const { createApp } = Vue
 
+
+
 createApp({
   data() {
     return {
       count: 0,
       page: "Home",
-      show: false
+      election: false,
+      candidates: [
+        {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:0},
+        {name: "Adolf", lastname:"Jk", party:"Berlin", voted: false, num_votes:0},
+        {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:0}
+      ]
     }
   }
 }).mount('#app')
