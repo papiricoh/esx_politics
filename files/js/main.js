25e457b4c8c5faeaa12d73eee68f86d454e4a343
papiricoh <slendeman0200@gmail.com>
2022-11-09 10:28:57 +0100

    Fixed election vote table

--- files/js/main.js
+++ files/js/main.js
@@ -5,26 +5,32 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+      debug_mode: true,
       count: 0,
       page: "Home",
+      is_senator: false,
+      is_president: false,
+      is_member_of_cabinet: false,
+      // Election Variables
       election: false,
       v_candidates: {
         results: [
-          {name: "juan", lastname:"Cuesta", party:"Cimadevilla", voted: false, num_votes:50},
-          {name: "Adolf", lastname:"Jk", party:"Berlin", voted: false, num_votes:420},
-          {name: "jerry", lastname:"Smith", party:"Albuquerque", voted: false, num_votes:20},
-          {name: "Rodrigo", lastname:"De Borbon", party:"Oviedin", voted: false, num_votes:20},
-          {name: "Juan", lastname:"Juanito", party:"JJ", voted: false, num_votes:20}
-      
+          { name: "juan", lastname: "Cuesta", party: "Cimadevilla", voted: false, num_votes: 50 },
+          { name: "Adolf", lastname: "Jk", party: "Berlin", voted: false, num_votes: 420 },
+          { name: "jerry", lastname: "Smith", party: "Albuquerque", voted: false, num_votes: 20 },
+          { name: "Rodrigo", lastname: "De Borbon", party: "Oviedin", voted: false, num_votes: 20 },
+          { name: "Juan", lastname: "Juanito", party: "JJ", voted: false, num_votes: 20 }
+
         ]
       },
       already_voted: false,
-      count: 0
+      // Senate Voting Variables
+
     }
   },
   methods: {
     vote() {
-      already_voted = true
-    }
+      this.already_voted = true
+    },
   },
 }).mount('#app')
