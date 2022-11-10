080311e32c14d7d28990419e29675dbc64a67d87
Papiricoh <slendeman0200@gmail.com>
2022-11-10 23:38:56 +0100

    Added law reader

--- files/js/main.js
+++ files/js/main.js
@@ -28,13 +28,16 @@ createApp({
       laws: {
         result: [
           { title: "Millitary Act", desc: "Desc", type: "Amendment", proposed_by: "Juan", for: 0, against: 0, abs: 0, in_active: false, is_signed: false, already_voted: 0 }, //Already voted 0:no, 1:for, 2:against, 3: abs
-          { title: "Constitucion of San Andreas", desc: "Desc", type: "Constitution", proposed_by: "Los Santos Council", for: 120, against: 34, abs: 32, in_active: true, is_signed: true, already_voted: 1 },
-          { title: "Free Minecraft Bill", desc: "Desc", type: "Bill", proposed_by: "Jeb", for: 120, against: 65, abs: 123, in_active: true, is_signed: false, already_voted: 2 }
+          { title: "Constitucion of San Andreas", desc: "First Article: HELLO", type: "Constitution", proposed_by: "Los Santos Council", for: 120, against: 34, abs: 32, in_active: true, is_signed: true, already_voted: 1 },
+          { title: "Free Minecraft Bill", desc: "Free minecraft for everyone", type: "Bill", proposed_by: "Jeb", for: 120, against: 65, abs: 123, in_active: true, is_signed: false, already_voted: 2 }
 
         ]
       },
       already_voted: false,
-      // Senate Voting Variables
+      // Law Reading
+      reading_law_title: "LAW_TITLE",
+      reading_law_type: "LAW_TYPE",
+      reading_law_desc: "LAW_DESC"
 
     }
   },
